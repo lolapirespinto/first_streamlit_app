058784892b96dd0f954466a75566887543bdaa79
Lola Pires Pinto <lolapinto@live.fr>
2024-01-15 16:16:02 +0100

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado test')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
