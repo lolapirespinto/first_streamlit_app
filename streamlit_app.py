e17b0f9d87049a5cea166af84937aae393726a63
Lola Pires Pinto <lolapinto@live.fr>
2024-01-15 16:14:34 +0100

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 streamlit.title('My parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
@@ -11,5 +14,4 @@ streamlit.text('🥑🍞 Avocado test')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
